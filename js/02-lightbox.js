eac60bf2eeb9a61fa829bbe524148205a2695550
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2022-12-15 23:35:15 +0200

    two

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const imagesMarkup = galleryHandler(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+function galleryHandler(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return '<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /></a >';
+        })
+        .join("");
+};
+
+galleryEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    };
+    const bigImage = e.target.getAttribute("data-source");
+    const instance = basicLightbox.create('<img src="${bigImage}" width="800"px height="600"px>');
+    instance.show();
+});
